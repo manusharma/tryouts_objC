8055324ed01e35693a424b7d609817ebedcbc5f4
Manu Sharma <msharma@blockdot.com>
2012-11-06 15:15:01 -0600

    Creating new Project

---
+++ navigation3/navigation3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  navigation3
+//
+//  Created by Manu Sharma on 11/6/12.
+//  Copyright (c) 2012 Manu Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ navigation3/navigation3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  navigation3
+//
+//  Created by Manu Sharma on 11/6/12.
+//  Copyright (c) 2012 Manu Sharma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
