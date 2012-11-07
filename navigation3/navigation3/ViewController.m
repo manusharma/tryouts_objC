9f014e30ed5642900c5692cdb9a372b19783dfd7
Manu Sharma <msharma@blockdot.com>
2012-11-07 01:15:21 -0600

    some new personalization in images

--- navigation3/navigation3/ViewController.m
+++ navigation3/navigation3/ViewController.m
@@ -22,11 +22,19 @@
     label.backgroundColor = [UIColor clearColor];
     //label.font = [UIFont boldSystemFontOfSize:20.0];
     label.font = [UIFont fontWithName:@"Lato" size:20.0];
-    label.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.5];
-    label.textAlignment = UITextAlignmentCenter;
+    //label.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+    //label.textAlignment = UITextAlignmentCenter;
     label.textColor =[UIColor whiteColor];
     label.text=self.title;
     self.navigationItem.titleView = label;
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0,0,100,30)];
+    btn.titleLabel.textColor = [UIColor whiteColor];
+    //self.navigationItem.backBarButtonItem = btn;
+    [self.modalButton.titleLabel setTextColor:[UIColor blackColor]];
+    [self.modalButton.titleLabel setFont:[UIFont fontWithName:@"Lato" size:14]];
+    CGFloat scale = [UIScreen mainScreen].scale;
+    //NSlog(@"%@", scale);
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +43,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)modalButton:(id)sender {
+}
+- (void)viewDidUnload {
+    [self setModalButton:nil];
+    [super viewDidUnload];
+}
 @end
